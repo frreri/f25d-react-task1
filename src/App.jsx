78d81ee5ffacdf4b5674725b5a825d8485960891
frreri <49623505+frreri@users.noreply.github.com>
2025-05-12 11:44:15 +0200

    Started building responsive navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 600px)");
+    const setMode = (e) => {
+      setIsMobile(e.matches);
+    };
+    mediaQuery.addEventListener("change", setMode);
+    setMode(mediaQuery);
+    return () => mediaQuery.removeEventListener("change", setMode);
+  }, []);
+
   return (
     <>
-      <NavBar />
+      <NavBar mobileMode={isMobile} />
     </>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,17 @@
 import NavItem from "./NavItem";
 import "./NavBar.css";
 
-function NavBar() {
+function NavBar({ mobileMode }) {
   return (
     <nav className="nav-bar">
       <h1>F25D React Task 1</h1>
-      <ul>
+      <ul
+        className={
+          mobileMode
+            ? "nav-menu nav-menu--mobile"
+            : "nav-menu nav-menu--desktop"
+        }
+      >
         <NavItem name="Home" url="#" />
         <NavItem name="About" url="#about" />
         <NavItem name="Contact Us" url="#contact" />
